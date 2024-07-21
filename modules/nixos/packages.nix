76c56edbcd1b47d0103259ff7f0d12165b02bc80
Ivan Ilak <ivan.ilak@hotmail.com>
2024-07-21 11:00:36 +0200

    Move some logic and add basis for nixOS

---
+++ modules/nixos/packages.nix
@@ -0,0 +1,49 @@
+{ pkgs }:
+
+with pkgs;
+let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
+shared-packages ++ [
+
+  # Security and authentication
+  keepassxc
+
+  # App and package management
+  gnumake
+  cmake
+  home-manager
+
+  # Media and design tools
+  vlc
+  fontconfig
+  font-manager
+
+  # Messaging and chat applications
+  discord
+
+  # Testing and development tools
+  rofi
+
+  # Screenshot and recording tools
+  flameshot
+
+  # Text and terminal utilities
+  feh # Manage wallpapers
+  tree
+  unixtools.ifconfig
+  unixtools.netstat
+  xorg.xrandr
+
+  # File and system utilities
+  inotify-tools # inotifywait, inotifywatch - For file system events
+  libnotify
+  xdg-utils
+
+  # Other utilities
+  firefox
+
+  # PDF viewer
+  zathura
+
+  # Music and entertainment
+  spotify
+]
--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,10 +1,9 @@
 { config, pkgs, lib, ... }:
 let
   user = "iilak";
-  xdg_configHome = "${config.users.users.${user}.home}/.config";
 in
 {
-  "${xdg_configHome}/alacritty".source = ../dotfiles/shared/alacritty;
-  "${xdg_configHome}/nvim".source = ../dotfiles/shared/nvim;
-  "${xdg_configHome}/cmake".source = ../dotfiles/shared/cmake;
+  "alacritty".source = ../dotfiles/shared/alacritty;
+  "nvim".source = ../dotfiles/shared/nvim;
+  "cmake".source = ../dotfiles/shared/cmake;
 }
