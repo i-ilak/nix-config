5c6f5e701b9c3319ad2089efa62b3960b6bea4bb
Ivan Ilak <ivan.ilak@hotmail.com>
2024-12-01 17:11:38 +0100

    Remove broken piece of code

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -57,11 +57,10 @@ in
             sharedFiles
             additionalFiles
           ];
+          programs = { };
 
           stateVersion = "24.05";
         };
-
-        programs = { } // import ../shared/home-manager.nix { inherit config pkgs lib; };
 
         # Marked broken Oct 20, 2022 check later to remove this
         # https://github.com/nix-community/home-manager/issues/3344
