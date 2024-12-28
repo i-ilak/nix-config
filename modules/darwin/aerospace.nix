90f67b93c6d0b89f8a4318c6b83c8c8a78efe641
Ivan Ilak <ivan.ilak@hotmail.com>
2024-12-28 16:45:23 +0100

    Small changes for macOS

--- modules/darwin/aerospace.nix
+++ modules/darwin/aerospace.nix
@@ -69,7 +69,7 @@ _:
             # Layout commands
             alt-slash = "layout accordion horizontal vertical";
             alt-backslash = "layout tiles horizontal vertical";
-            alt-enter = "exec-and-forget /Users/iilak/.nix-profile/bin/alacritty";
+            alt-enter = "exec-and-forget open -n /Applications/Ghostty.app";
 
             # Fullscreen commands
             alt-shift-q = "close";
