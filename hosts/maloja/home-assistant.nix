6237562f3f38c221dd3b61dd6b667879e17234ce
Ivan Ilak <ivan.ilak@hotmail.com>
2025-08-24 19:44:23 +0200

    Initial implementation of backup & restore for Home Assistant

--- hosts/maloja/home-assistant.nix
+++ hosts/maloja/home-assistant.nix
@@ -26,72 +26,65 @@
       };
     };
   };
-  # systemd.services.home-assistant-restore-from-backup =
-  #   let
-  #     baseDirPaperless = "/var/lib/hass";
-  #   in
-  #   {
-  #     description = "Restore Home Assistant from S3 backup if not already done";
-  #
-  #     wantedBy = [ "multi-user.target" ];
-  #     wants = [
-  #       "network-online.target"
-  #       "var-lib-hass.mount"
-  #     ];
-  #     after = [
-  #       "network-online.target"
-  #     ];
-  #     before = [
-  #       "home-assistant.service"
-  #     ];
-  #     unitConfig = {
-  #       ConditionPathExists = "!${baseDirPaperless}/.restore_service_completed";
-  #     };
-  #
-  #     serviceConfig = {
-  #       Type = "oneshot";
-  #       User = "root";
-  #       Group = "backup";
-  #       EnvironmentFile = config.sops.templates."accessFile".path;
-  #       WorkingDirectory = "${baseDirPaperless}";
-  #     };
-  #
-  #     script =
-  #       # bash
-  #       ''
-  #         RESTORE_COMPLETION_FLAG="${baseDirPaperless}/.restore_service_completed"
-  #
-  #         if [ -f "$RESTORE_COMPLETION_FLAG" ]; then
-  #           echo "Restore completion flag found ($RESTORE_COMPLETION_FLAG). Skipping restore."
-  #           exit 0
-  #         fi
-  #
-  #         echo "--- Starting Paperless-NGX restore from S3 backup ---"
-  #
-  #         echo "Restoring latest backup to ${backupDir}..."
-  #         ${pkgs.restic}/bin/restic                                               \
-  #           --repository-file "${config.sops.templates."repositoryFile".path}"    \
-  #           --password-file "${config.sops.secrets."restic-password-file".path}"  \
-  #           restore latest:${backupDir} --target ${backupDir}
-  #
-  #         chown -R paperless:paperless "${backupDir}"
-  #
-  #         echo "Importing documents into Paperless-NGX..."
-  #         ${pkgs.su}/bin/su -s /bin/sh paperless -c "
-  #           export PAPERLESS_DATA_DIR=${dataDir}
-  #           export PAPERLESS_MEDIA_ROOT=${mediaDir}
-  #           export PAPERLESS_CONSUMPTION_DIR=${dataDir}/consume
-  #           ${pkgs.paperless-ngx}/bin/paperless-ngx migrate
-  #           ${pkgs.paperless-ngx}/bin/paperless-ngx document_importer '${backupDir}'
-  #         "
-  #
-  #         echo "Cleaning up temporary directory..."
-  #         rm -rf "$RESTORE_DIR"
-  #
-  #         echo "Creating restore completion flag file: $RESTORE_COMPLETION_FLAG."
-  #         touch "$RESTORE_COMPLETION_FLAG"
-  #         chown "${config.services.paperless.user}:paperless" "$RESTORE_COMPLETION_FLAG"
-  #         echo "--- Paperless-NGX restore complete ---"
-  #       '';
-  #   };
+
+  systemd.services.home-assistant-restore-from-backup =
+    let
+      baseDirHomeAssistant = "/var/lib/hass";
+      backupDir = "${baseDirHomeAssistant}/backups";
+    in
+    {
+      description = "Restore Home Assistant from S3 backup if not already done";
+
+      wantedBy = [ "multi-user.target" ];
+      wants = [
+        "network-online.target"
+        "var-lib-hass.mount"
+      ];
+      after = [
+        "network-online.target"
+      ];
+      before = [
+        "home-assistant.service"
+      ];
+      unitConfig = {
+        ConditionPathExists = "!${baseDirHomeAssistant}/.restore_service_completed";
+      };
+
+      serviceConfig = {
+        Type = "oneshot";
+        User = "root";
+        Group = "backup";
+        EnvironmentFile = config.sops.templates."home-assistant-accessFile".path;
+        WorkingDirectory = "${baseDirHomeAssistant}";
+      };
+
+      script =
+        # bash
+        ''
+          RESTORE_COMPLETION_FLAG="${baseDirHomeAssistant}/.restore_service_completed"
+
+          if [ -f "$RESTORE_COMPLETION_FLAG" ]; then
+            echo "Restore completion flag found ($RESTORE_COMPLETION_FLAG). Skipping restore."
+            exit 0
+          fi
+
+          echo "--- Starting Home Assistant restore from S3 backup ---"
+
+          echo "Restoring latest backup to ${backupDir}..."
+          restic-home-assistant restore latest:${backupDir} --target ${backupDir}
+            
+          chown -R "${config.services.home-assistant.user}:${config.services.home-assistant.group}" "${backupDir}"
+
+          NEWEST_SLUG=$(ha --config ${baseDirHomeAssistant}/configuration.yaml backups list | grep -v "SLUG" | head -1 | awk '{print $1}')
+          echo "The newst slug is: $NEWEST_SLUG"
+          echo "Importing data into Home Assistant..."
+          ha --config ${baseDirHomeAssistant}/configuration.yaml backups restore $NEWEST_SLUG
+
+
+          echo "Creating restore completion flag file: $RESTORE_COMPLETION_FLAG."
+          touch "$RESTORE_COMPLETION_FLAG"
+          chown "${config.services.home-assistant.user}:${config.services.home-assistant.group}" "$RESTORE_COMPLETION_FLAG"
+          echo "--- Home Assistant restore complete ---"
+        '';
+    };
 }
