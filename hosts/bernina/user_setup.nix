da845232d40b23338383ab4c18151ecf6f873e1f
Ivan Ilak <ivan.ilak@hotmail.com>
2025-10-06 21:42:42 +0200

    Forgot attribute

--- hosts/bernina/user_setup.nix
+++ hosts/bernina/user_setup.nix
@@ -12,7 +12,7 @@
     users = {
       root = {
         hashedPasswordFile = config.sops.secrets."user-root-password".path;
-        openssh.authorizedKeys = [
+        openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOTlQXyssgPreQcwbsg54nMZUQD9IVIDEmfKn4ssXCfx"
         ];
       };
